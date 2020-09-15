f3e99f66b7eb5422b0d6da6506bcce9f398843ad
Nico Elzer <contact@nicoelzer.de>
2020-09-15 16:53:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# DXswap History
+
+Transaction and liquidity pool stats for [DXswap](https://dxswap.eth.link).
+
+Check it out live: [https://dxswap.info](https://dxswap.info).
+
+### To Start Development
+
+###### Installing dependencies
+```bash
+yarn
+```
+
+###### Running locally
+```bash
+yarn start
+```
---
+++ src/components/Attribution/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/nicoelzer/dxswap-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://dxswap.eth.link" rel="noopener noreferrer" target="_blank">
+      DXswap
+    </a>{' '}
+  </p>
+)
+
+export default Attribution

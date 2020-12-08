da9e5e7b8adac5f8c984df8c9263cf02dc22252b
nicoelzer <contact@nicoelzer.de>
2020-12-08 15:36:01 +0100

    removed token lists

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,12 +10,6 @@ export const timeframeOptions = {
   ALL_TIME: 'All time'
 }
 
-// token list urls to fetch tokens from - use for warnings on tokens and pairs
-export const SUPPORTED_LIST_URLS__NO_ENS = [
-  'https://gateway.ipfs.io/ipns/tokens.uniswap.org',
-  'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json'
-]
-
 // hide from overview list
 export const OVERVIEW_TOKEN_BLACKLIST = [
   '0x495c7f3a713870f68f8b418b355c085dfdc412c3',

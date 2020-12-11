0ea3c30a3a3ec9ad4ed5deb93fbcf190246533d7
nicoelzer <contact@nicoelzer.de>
2020-12-11 14:19:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Swapr History
+
+Transaction and liquidity pool stats for [Swapr](https://swapr.eth.link).
+
+Check it out live: [https://swapr.info](https://swapr.info).
+
+### To Start Development
+
+###### Installing dependencies
+
+```bash
+yarn
+```
+
+###### Running locally
+
+```bash
+yarn start
+```
---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/nicoelzer/swapr-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://swapr.eth.link" rel="noopener noreferrer" target="_blank">
+      Swapr
+    </a>{' '}
+    |{' '}
+    <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution

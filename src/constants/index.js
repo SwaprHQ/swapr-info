582b8d6371f4e13979058a78be7bd3b1d84d2589
Federico Luzzi <fedeluzzi00@gmail.com>
2021-08-30 14:20:14 +0200

    fix: remove memory leak from token icon (improve general performances)

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,6 +16,12 @@ export const SupportedNetworkForChainId = Object.freeze({
   [ChainId[SupportedNetwork.ARBITRUM_ONE]]: SupportedNetwork.ARBITRUM_ONE,
 });
 
+export const ChainIdForSupportedNetwork = Object.freeze({
+  [SupportedNetwork.MAINNET]: ChainId[SupportedNetwork.MAINNET],
+  [SupportedNetwork.XDAI]: ChainId[SupportedNetwork.XDAI],
+  [SupportedNetwork.ARBITRUM_ONE]: ChainId[SupportedNetwork.ARBITRUM_ONE],
+});
+
 export const FACTORY_ADDRESS = {
   [SupportedNetwork.MAINNET]: "0xd34971BaB6E5E356fd250715F5dE0492BB070452",
   [SupportedNetwork.XDAI]: "0x5d48c95adffd4b40c1aaadc4e08fc44117e02179",

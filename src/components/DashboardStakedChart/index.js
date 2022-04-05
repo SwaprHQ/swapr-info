4069cda18c893d6703e93ecf9941e74f54d84a1d
Paolo Guerra <pg.paologuerra@gmail.com>
2022-04-05 22:15:47 +0200

    refactor: remove unused charts

--- src/components/DashboardStakedChart/index.js
+++
@@ -1,7 +0,0 @@
-import BaseChart from "./BaseChart";
-import ResponsiveChart from "./ResponsiveChart";
-
-export {
-  BaseChart as StackedBaseChart,
-  ResponsiveChart as ResponsiveStackedChart,
-};
